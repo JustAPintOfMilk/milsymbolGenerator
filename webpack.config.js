68fb6edc3ecea042206b61e2d9fa679cd4d0d2fe
Christoph Witte <chris.witte.andi@gmail.com>
2023-08-06 16:32:29 +0200

    automatic reload on change for webpack development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,42 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlInlineScriptPlugin  = require('html-inline-script-webpack-plugin');
-const { spawn } = require('child_process')
-const webpack = require('webpack')
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const ReactRefreshTypeScript = require('react-refresh-typescript');
 
 const mode = env => env.production ? 'production' : 'development'
 
-const RULES = [
+const devServer = env => env.production ? {} : {
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '.webpack'),
+            watch: false
+        },
+        devMiddleware: {
+            index: true,
+            mimeTypes: { phtml: 'text/html' },
+            writeToDisk: true,
+        },
+        watchFiles: ['src'],
+        port: 3000,
+        open: true
+    }
+}
+const getDevPlugins = env => env.production ? [] : [
+    new ReactRefreshWebpackPlugin()
+]
+const getTsLoaderExtension = env => env.production ? {} : {
+    options: {
+        getCustomTransformers: () => ({
+            before: [ReactRefreshTypeScript()]
+        }),
+        transpileOnly: true
+    }
+}
+
+
+const getRules = (env) => [
     {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -25,25 +54,27 @@ const RULES = [
                 name: 'img/[name].[ext]'
             }
         }]
+    },
+    {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'ts-loader',
+            ...getTsLoaderExtension(env)
+        }
     }
 ]
 const rendererConfig = (env) => ({
     output: {
         path: path.resolve(__dirname, '.webpack'),
-        publicPath:"./"
+        publicPath: "./"
     },
     context: path.resolve(__dirname, 'src'),
     target: 'web',
     mode: mode(env),
     stats: 'errors-only',
     module: {
-        rules: [...RULES, {
-            test: /\.tsx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'ts-loader'
-            }
-        }]
+        rules: getRules(env)
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.jsx', '.js']
@@ -52,13 +83,19 @@ const rendererConfig = (env) => ({
         renderer: ['./index.tsx']
     },
     plugins: [
-        
         new HtmlWebpackPlugin({
             title: "MS-Generator",
-            inject:'body'
+            inject: 'body'
         }),
-        new HtmlInlineScriptPlugin()
-    ]
+        new HtmlInlineScriptPlugin(),
+        ...getDevPlugins(env)
+    ],
+    ...devServer(env)
+
 })
 
-module.exports = (env = {}) => [rendererConfig(env)]
+
+module.exports = (env = {}, argv) => {
+    env = { ...env, production: argv.mode === 'production' }
+    return [rendererConfig(env)]
+}
